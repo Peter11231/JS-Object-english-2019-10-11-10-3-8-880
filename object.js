0f6e429a949bd555d33cfb1f49f24fa7340f83e1
Peter11231 <92013839+Peter11231@users.noreply.github.com>
2021-10-14 20:31:21 +0800

    Create object.js

---
+++ object.js
@@ -0,0 +1,15 @@
+//Q1
+var user = {};
+user.name="John";
+user.surname = "Mike";
+user.name="Peter"
+delete user.name;
+
+//Q2
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+    };
+    // write your code here..
+    console.log(fruit.apple+fruit.peach+fruit.pear);
